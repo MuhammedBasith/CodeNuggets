083eeb6b73e259c524664c7b6572b6c9f1f7fb01
MuhammedBasith <iambasith123@gmail.com>
2024-07-15 20:50:20 +0300

    Change in Number

--- src/components/component/new-signup-form.tsx
+++ src/components/component/new-signup-form.tsx
@@ -78,7 +78,7 @@ function SignupFormContent() {
   };
 
   const handleAddToWhatsApp = () => {
-    const whatsappLink = `https://wa.me/918547812187?text=Hey%20Team%20CodeNuggets,%0D%0AI%20have%20just%20registered%20for%20the%20Python%20class%20but%20my%20payment%20is%20pending.%0D%0AMy%20email%20ID%20is%20${encodeURIComponent(formData.email)}.%0D%0APlease%20let%20me%20know%20the%20steps%20to%20complete%20the%20payment.%0D%0AThanks!`;
+    const whatsappLink = `https://wa.me/919562304928?text=Hey%20Team%20CodeNuggets,%0D%0AI%20have%20just%20registered%20for%20the%20Python%20class%20but%20my%20payment%20is%20pending.%0D%0AMy%20email%20ID%20is%20${encodeURIComponent(formData.email)}.%0D%0APlease%20let%20me%20know%20the%20steps%20to%20complete%20the%20payment.%0D%0AThanks!`;
     window.location.href = whatsappLink;
   };
 
