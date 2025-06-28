7fea74b348e29363e4125b2a8ff6931acb5495bd
MuhammedBasith <iambasith123@gmail.com>
2025-06-28 18:17:05 +0530

    add opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,30 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "CodeNuggets",
-  description: "Elevating your Software Career",
+  description: "Join our exclusive community of learners today and elevate your skills to become a part of the top 1% of programmers.",
+  openGraph: {
+    title: "CodeNuggets - Become a Top 1% Engineer",
+    description: "Join our exclusive community of learners today and elevate your skills to become a part of the top 1% of programmers.",
+    url: "https://codenuggets.vercel.app",
+    siteName: "CodeNuggets",
+    images: [
+      {
+        url: "/opengraph.png",
+        width: 1200,
+        height: 630,
+        alt: 'CodeNuggets - Elevate Your Software Career',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'CodeNuggets - Become a Top 1% Engineer',
+    description: 'Join our exclusive community of learners today and elevate your skills to become a part of the top 1% of programmers.',
+    images: ['/opengraph.png'],
+  },
+  metadataBase: new URL('https://codenuggets.vercel.app'),
 };
 
 export default function RootLayout({
